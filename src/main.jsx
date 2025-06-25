5811786a0b2af24da7bebd97038b7ca101984b86
KerD <Kost753@bk.ru>
2025-06-25 20:49:29 +0300

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import WishFlickPlatform from './WishFlickPlatform';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <WishFlickPlatform />
-  </React.StrictMode>
-);
